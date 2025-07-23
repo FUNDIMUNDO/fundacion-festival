22e609b4a0560cb1463fbea9c00cdf2cafd57ce0
FUNDIMUNDO <carnabogsn@gmail.com>
2025-07-23 12:00:12 -0500

    ✅ Validar y confirmar configuración de Firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,18 +1,18 @@
 // src/firebaseConfig.js
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getAuth }       from 'firebase/auth';
 import { getFirestore }  from 'firebase/firestore';
 import { getStorage }    from 'firebase/storage';
 
 // Configuración de Firebase (SDK JS v7.20.0+)
 const firebaseConfig = {
-  apiKey: "AIzaSyAleN4JRv5oUHfs_l7AWeoBzFCdSSjqfMA",
-  authDomain: "fundimundo-4fbce.firebaseapp.com",
-  projectId: "fundimundo-4fbce",
-  storageBucket: "fundimundo-4fbce.appspot.com",
-  messagingSenderId: "722547545080",
-  appId: "1:722547545080:web:c67538df57e22b0769bc0d",
-  measurementId: "G-88LGL9FHXP"
+  apiKey: 'AIzaSyAleN4JRv5oUHfs_l7AWeoBzFCdSSjqfMA',
+  authDomain: 'fundimundo-4fbce.firebaseapp.com',
+  projectId: 'fundimundo-4fbce',
+  storageBucket: 'fundimundo-4fbce.appspot.com',
+  messagingSenderId: '722547545080',
+  appId: '1:722547545080:web:c67538df57e22b0769bc0d',
+  measurementId: 'G-88LGL9FHXP'
 };
 
 // Inicializar SDK
